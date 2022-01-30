705f5ca60e3e5d91ce8c40c70ec61e9891fcfdb9
StefanoZiinoC <sziinocolanino@gmail.com>
2022-01-30 16:19:51 -0300

    first commit

---
+++ src/routes/employees.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express')
+const router = Router()
+
+const employeesCtrl = require('../controllers/employees.controller.js')
+
+//crud
+// Create Read Update/Patch Delete
+
+router.get('/', employeesCtrl.getEmployees);
+router.post('/', employeesCtrl.createEmployees);
+router.get('/:id', employeesCtrl.getEmployee);
+router.put('/:id', employeesCtrl.editEmployees);
+router.delete('/:id', employeesCtrl.deleteEmployees);
+
+module.exports = router
